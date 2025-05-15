2a9c304aea61e36036423ca652c546a044d0eb3e
violetaduro <violetadrf@gmail.com>
2025-05-15 21:08:13 +0000

    Solved the problems with lemmatized_doc and infiltred ghibli movie

--- topic_modelling.py
+++ topic_modelling.py
@@ -27,6 +27,7 @@ stop_words = set(stopwords.words('english'))
 
 # Read all files in the data folder
 raw_documents = []
+movie_titles = []
 
 for filename in os.listdir(data_folder):
     file_path = os.path.join(data_folder, filename)
@@ -34,6 +35,7 @@ for filename in os.listdir(data_folder):
         with open(file_path, 'r', encoding='utf-8') as f:
             words = f.read().split()
             raw_documents.append(words)
+            movie_titles.append(filename.replace('.txt', ''))
 
 
 # Lemmatize with POS awareness
@@ -52,30 +54,49 @@ for words in raw_documents:
             lemmatized_doc.append(lemma)
     movie_docs.append(lemmatized_doc)
 
-# ''' Construct the document-term matrix '''
+ #''' Construct the document-term matrix '''
 # # Imports
-# from gensim import corpora
-# import itertools
-# from collections import Counter
-# from gensim import models
-# from gensim.models.ldamodel import LdaModel
+from gensim import corpora
+import itertools
+from collections import Counter
+from gensim import models
+from gensim.models.ldamodel import LdaModel
 
 # # Filtering out rare and common words based on their frequency in the document 
 # # !!! I have randomly chosen 5 and 500 for now --> needs to be changed 
-# word_counts = Counter(lemmatized_doc)
-# filtered_doc = [word for word in lemmatized_doc if 5 <= word_counts[word] <= 500]
+all_words = list(itertools.chain.from_iterable(movie_docs))
+word_counts = Counter(all_words)
 
-# # Create pseudo-documents by splitting into chunks, as LDA expects multiple documents
-# chunk_size = 100  
-# pseudo_docs = [filtered_doc[i:i + chunk_size] for i in range(0, len(filtered_doc), chunk_size)]
+# Filter each document individually using global word frequencies
+filtered_docs = []
+for doc in movie_docs:
+    filtered = [word for word in doc if 5 <= word_counts[word] <= 500]
+    if filtered:
+        filtered_docs.append(filtered)
 
-# # Remove empty chunks just in case
-# pseudo_docs = [doc for doc in pseudo_docs if doc]
+movie_dictionary = corpora.Dictionary(filtered_docs)
+movie_bow = [movie_dictionary.doc2bow(doc) for doc in filtered_docs]
 
-# movie_dictionary = corpora.Dictionary(pseudo_docs)
-# movie_bow = [movie_dictionary.doc2bow(doc) for doc in pseudo_docs]
-# print(movie_bow[0][:50])
+print(movie_bow[0][:50])
 
 # # Applying the LDA model
-# movie_ldamodel = models.ldamodel.LdaModel(movie_bow, num_topics=5, id2word = movie_dictionary, passes = 20)
-# print(movie_ldamodel.show_topics(formatted=False, num_words=5))
+movie_ldamodel = models.ldamodel.LdaModel(movie_bow, num_topics=5, id2word = movie_dictionary, passes = 20)
+# Show topics
+print("Model topics:")
+for topic in movie_ldamodel.show_topics(formatted=False, num_words=5):
+    print(topic)
+
+# Get the dominant topic per movie:
+print("\nDominant topic per movie:")
+for i, bow in enumerate(movie_bow):
+    topics = movie_ldamodel.get_document_topics(bow)
+    if topics:
+        dominant_topic = max(topics, key=lambda x: x[1])
+        print(f"{movie_titles[i]}: Dominant topic {dominant_topic[0]}")
+    else:
+        print(f"Movie {i}: No dominant topic assigned")
+
+# preview the first processed script (just in case)
+
+print("\nExample of processed script:", movie_docs[0][:50])
+
